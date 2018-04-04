d552f7cdb8e0b88008faf3a37b30e119069cc78f
skmbw <tongku2008@126.com>
2018-04-04 15:26:10 +0800

    添加搜索模块，代码调整

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -31,4 +31,20 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeros().subscribe(heros => this.heros = heros);
     // this.heros = this.heroService.getHeros();
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heros.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heros = this.heros.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
